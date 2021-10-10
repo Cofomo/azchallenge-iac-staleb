395ebb7405d5174f217fa63d256c69f369e1f794
Said Taleb <said@inovasquad.com>
2021-10-09 21:46:38 -0400

    Ajouter les fichiers de l'épreuve 1

---
+++ variables.tf
@@ -0,0 +1,13 @@
+variable "resource-group-name"{
+    default = "azchallenge-staleb-dev-rg"
+    description = "The name of resource group"
+}
+
+variable "location" {
+    default = "Canada Central"
+    description = "The azure location"
+}
+
+variable "environment" {
+    default = "dev"
+}
