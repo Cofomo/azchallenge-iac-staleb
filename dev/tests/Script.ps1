3072db9d451550f8afc31aed604f92b2aacbb3f8
Said Taleb <said@inovasquad.com>
2021-12-12 06:10:41 -0500

    add tests to workflow

--- dev/tests/Script.ps1
+++ dev/tests/Script.ps1
@@ -1,7 +1,7 @@
 Describe "verify-Configuration-Function" {
 
     It "Verify the Location" {
-        $functionInfo = Get-AzWebApp -ResourceGroupName azchallenge-staleb-prod-rg -Name calicot-function-app-prod
+        $functionInfo = Get-AzWebApp -ResourceGroupName azchallenge-staleb-dev-rg -Name calicot-function-app-dev
         $functionInfo.Location | Should -Be 'Canada Central' 
     }
 
